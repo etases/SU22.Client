9e0d84b849f44bb18cf082e7b0f1f81cef94d561
HSGamer <huynhqtienvtag@gmail.com>
2022-07-04 17:21:30 +0700

    polish

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,4 +1,4 @@
-import { Grid, Text, UnstyledButton } from '@mantine/core'
+import { Container, Grid, Group, Stack, Text, UnstyledButton } from '@mantine/core'
 import { Link, Outlet, useParams } from 'react-router-dom'
 
 const { Col } = Grid
@@ -6,8 +6,18 @@ const { Col } = Grid
 function CategoryElement(elementProps) {
 	return (
 		<UnstyledButton component={Link} to={'./' + elementProps.id}>
-			<Text>Line1</Text>
-			<Text size='xs' color='gray'>Line2</Text>
+			<div style={{
+				paddingLeft: "2px",
+				backgroundColor: "#f5f5f5",
+				borderRadius: "4px",
+				border: "1px solid #eaeaea",
+			}}>
+				<Text size='xs' color='gray'>User abc asked: </Text>
+				<Text lineClamp={2}>
+					Lor ipsum dolor sit amet, consectetur adipiscing elit.
+					Lor ipsum dolor sit amet, consectetur adipiscing elit.
+				</Text>
+			</div>
 		</UnstyledButton>
 	)
 }
@@ -17,9 +27,11 @@ export function Category() {
 	return (
 		<Grid grow={true}>
 			<Col style={{ outline: '1px solid black' }} span={2}>
-				<CategoryElement id={12345} />
-				<CategoryElement id={2468} />
-				<CategoryElement id={1111} />
+				<Stack spacing="xs">
+					<CategoryElement id={12345} />
+					<CategoryElement id={2468} />
+					<CategoryElement id={1111} />
+				</Stack>
 			</Col>
 			<Col style={{ outline: '1px solid red' }} span={8}>
 				<Outlet />
