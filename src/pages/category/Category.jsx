893ed5aba65fbbabfa4c7943922085c3507dede2
Huynh Tien <huynhqtienvtag@gmail.com>
2022-07-06 16:23:14 +0700

    Category + Topic layout (#5)
    * first elements
    
    * polish
    
    * style the topic
    
    * style the topic
    
    * translation

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,16 +1,48 @@
-import { Grid } from '@mantine/core'
-import { Outlet, useParams } from 'react-router-dom'
+import { Grid, Stack, Text, UnstyledButton } from '@mantine/core'
+import { Link, Outlet, useParams } from 'react-router-dom'
+import { useTranslation } from '~/hooks'
 
 const { Col } = Grid
 
+function TopicComponent(elementProps) {
+	const askedMsgTranslation = useTranslation({
+		translationKey: 'topic.user_asked',
+		keyParams: {
+			name: '123',
+		},
+	})
+	return (
+		<UnstyledButton component={Link} to={'./' + elementProps.id}>
+			<div style={{
+				paddingLeft: "2px",
+				backgroundColor: "#f5f5f5",
+				borderRadius: "4px",
+				border: "1px solid #eaeaea",
+			}}>
+				<Text size='xs' color='gray'>{askedMsgTranslation.error || askedMsgTranslation.value}</Text>
+				<Text lineClamp={2}>
+					Lor ipsum dolor sit amet, consectetur adipiscing elit.
+					Lor ipsum dolor sit amet, consectetur adipiscing elit.
+				</Text>
+			</div>
+		</UnstyledButton>
+	)
+}
+
 export function Category() {
-  const { category } = useParams()
-  return (
-    <Grid grow={true}>
-      <Col span={6}>{category}</Col>
-      <Col span={6}>
-        <Outlet />
-      </Col>
-    </Grid>
-  )
+	const { category } = useParams()
+	return (
+		<Grid grow={true}>
+			<Col style={{ outline: '1px solid black' }} span={2}>
+				<Stack spacing="xs">
+					<TopicComponent id={12345} />
+					<TopicComponent id={2468} />
+					<TopicComponent id={1111} />
+				</Stack>
+			</Col>
+			<Col style={{ outline: '1px solid red' }} span={8}>
+				<Outlet />
+			</Col>
+		</Grid>
+	)
 }
--- src/pages/topic/Topic.jsx
+++ src/pages/topic/Topic.jsx
@@ -1,6 +1,83 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { Button, Group, Paper, Stack, Text, TextInput, UnstyledButton} from '@mantine/core'
+import { useTranslation } from '~/hooks'
+
+function Comment(commentProps) {
+	const saidMsgTranslation = useTranslation({
+		translationKey: 'topic.user_said',
+		keyParams: {
+			name: commentProps.name,
+		},
+	});
+	return (
+		<UnstyledButton component={Link} to={"../" + commentProps.id}>
+			<Text size='xs' color='gray'>{saidMsgTranslation.error || saidMsgTranslation.value}</Text>
+			<Text lineClamp={2}>
+				Lor ipsum dolor sit amet, consectetur adipiscing elit.
+				Lor ipsum dolor sit amet, consectetur adipiscing elit.
+			</Text>
+		</UnstyledButton>
+	)
+}
 
 export function Topic() {
-	const { topic } = useParams()
-	return <>{topic}</>
+	const { topic } = useParams();
+	const askedMsgTranslation = useTranslation({
+		translationKey: 'topic.user_asked',
+		keyParams: {
+			name: '123',
+		},
+	});
+	const backParentTranslation = useTranslation({ translationKey: 'topic.back_to_parent' });
+	return (
+		<Stack spacing={10}>
+			{/* Header */}
+
+			{/* Topic / Parent Comment */}
+			<Stack spacing={10} style={{
+				border: `1px solid black`,
+				borderRadius: "4px"
+			}}>
+				<Group position={'apart'} style={{ padding: "8px" }}>
+					<Text weight={500}>{askedMsgTranslation.error || askedMsgTranslation.value}</Text>
+					<Button variant={'subtle'}>{backParentTranslation.error || backParentTranslation.value}</Button>
+				</Group>
+				<Paper p="lg">
+					<Text>Paper is the most basic ui component</Text>
+					<Text>
+						This is the topic: {topic}
+					</Text>
+				</Paper>
+				<Group position={'apart'} style={{ padding: "8px" }}>
+					<Text color="dimmed">Keyword</Text>
+					<Button radius="xl" size="xs" uppercase>
+						??
+					</Button>
+				</Group>
+			</Stack>
+
+			{/* Comments */}
+			<Stack style={{
+				padding: "8px",
+				border: `1px solid black`,
+				borderRadius: "4px"
+			}}>
+				{/* Add Comment */}
+				<Group grow={true} style={{
+					padding: "8px",
+					border: `1px solid black`,
+					borderRadius: "4px"
+				}}>
+					<TextInput label="Add Comment" placeholder="Add your comment here" />
+				</Group>
+
+				{/* Show Comments */}
+				<Stack>
+					<Comment name={"Test1"} id={"123"}/>
+					<Comment name={"Test2"} id={"1234"}/>
+					<Comment name={"Test3"} id={"12356"}/>
+				</Stack>
+			</Stack>
+		</Stack>
+	)
 }
